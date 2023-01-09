4b56b5b693339ea143b5c05ae1b770d3d9a0b85a
activate-glacier-instinct <101533082+activate-glacier-instinct@users.noreply.github.com>
2023-01-09 22:22:08 +0000

    FEAT (components/nav) : started breaking up the components
    Created /components
    
    Made the nav components within /components

---
+++ src/components/+nav.svelte
@@ -0,0 +1,21 @@
+<nav class="layout__nav">
+	<a href="/">home</a>
+</nav>
+
+<style>
+	.layout__nav {
+		display: flex;
+		flex-direction: row-reverse;
+        position: absolute;
+        margin: 2rem 2rem;
+        top: 0;
+        right: 0;
+	}
+	.layout__nav > a {
+		font-size: 1.5rem;
+		font-weight: 700;
+		text-transform: uppercase;
+		text-decoration: none;
+        color: var(--theme-color-mango)
+	}
+</style>

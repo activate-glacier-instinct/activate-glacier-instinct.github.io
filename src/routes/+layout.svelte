3cdfbb92fa3288d44a0487cbdfe06f1d3d195be7
activate-glacier-instinct <101533082+activate-glacier-instinct@users.noreply.github.com>
2023-01-09 22:24:13 +0000

    FEAT (layout): updates the main layout
    Adds the new Nav component
    
    Wrapps the <slot> and styled it - should abstract it next
    
    Added color variables
    
    Added body styling
    
    Added body font-size for implementing relative values
    
    A bit of color pallete research

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,44 @@
 <script>
 	export const prerender = true;
-    export const trailingSlash = 'always';
+	export const trailingSlash = 'always';
+	import Nav from '../components/+nav.svelte';
 </script>
 
-<nav>
-	<a href="/">home</a>
-</nav>
+<Nav />
 
-<slot />
+<main class="layout__main">
+	<slot />
+</main>
+
+<style>
+	.layout__main {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		max-width: 1024px;
+		margin: 0 auto;
+		padding: 2rem;
+	}
+	:global(body) {
+		--theme-color-mango: #f7b801;
+		--theme-color-purple: #3d348b;
+		--theme-color-purple-light: #7678ed;
+		--theme-color-tangerine: #f18701;
+		--theme-color-jet-black: #30323d;
+		box-sizing: border-box;
+		margin: 0;
+		height: 100vh;
+		width: 100vw;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		position: absolute;
+		background-color: var(--theme-color-jet-black);
+		color: var(--theme-color-purple-light);
+		transition: background-color 0.3s;
+		font-size: 16px;
+	}
+	:global(body.dark-mode) {
+		/* TODO: add dark-mode toggle */
+	}
+</style>

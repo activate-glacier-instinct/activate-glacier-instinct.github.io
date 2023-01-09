7852690635a30fcfa38b577b62eee25ed9c2b12b
activate-glacier-instinct <101533082+activate-glacier-instinct@users.noreply.github.com>
2023-01-09 22:32:48 +0000

    STYLE: adds some styles to the front page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,14 @@
-<h1>I'm activate-glacier-instinct</h1>
+<h1>I'm <i class='accent'>activate-glacier-instinct</i>,</h1>
 <h2>Fullstack Web Developer learning smart contract development with Solidity</h2>
 <p>Read more about my experience, my goals, and my progress on 100days-of-code</p>
 
+<style>
+    h1, h2, p {
+        font-size: 2rem;
+        font-weight: 900;
+    }
+    .accent {
+        font-style: normal;
+        color: var(--theme-color-mango);
+    }
+</style>

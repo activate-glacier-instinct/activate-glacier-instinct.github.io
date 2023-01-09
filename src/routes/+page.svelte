0a2decf8738f49b9cc8d9a21d9c5ba2a90f246b9
activate-glacier-instinct <101533082+activate-glacier-instinct@users.noreply.github.com>
2023-01-09 22:23:00 +0000

    CONTENT: updates the main page content and updates the playwright test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,4 @@
-
-<h1>Hello,</h1>
-<h2>I'm activate-glacier-instinct</h2>
+<h1>I'm activate-glacier-instinct</h1>
 <h2>Fullstack Web Developer learning smart contract development with Solidity</h2>
 <p>Read more about my experience, my goals, and my progress on 100days-of-code</p>
 

420910f448b8267ea3811f1b296c760914e1fce3
activate-glacier-instinct <101533082+activate-glacier-instinct@users.noreply.github.com>
2023-01-12 23:38:15 +0000

    REFACTOR : moved the P5 backdrop to another component
    Simplified the layout compoenent

---
+++ src/components/+backdrop.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+	import P5, { type Sketch } from 'p5-svelte';
+
+	const sketch: Sketch = (p5) => {
+		let y = 100;
+		let z = 100;
+		let q = 100;
+		const padding = 8;
+		// The statements in the setup() function
+		// execute once when the program begins
+		p5.setup = () => {
+			// createCanvas must be the first statement
+			p5.createCanvas(p5.windowWidth - padding, p5.windowHeight - padding);
+			p5.stroke(247, 184, 1); // Set line drawing color to white
+			p5.frameRate(160);
+		};
+		// The statements in draw() are executed until the
+		// program is stopped. Each statement is executed in
+		// sequence and after the last line is read, the first
+		// line is executed again.
+		p5.draw = () => {
+			p5.background(48, 50, 61); // Set the background to black
+
+			y = createLines(y, 10);
+			q = createLines(q, 5);
+			z = createLines(z, 7.5);
+		};
+		function createLines (linePoint: number, steps: number) {
+			linePoint -= steps
+			if (linePoint < 0) {
+				linePoint = p5.height
+			}
+			p5.line(0, linePoint, p5.width, linePoint);
+			return linePoint;
+		}
+
+	};
+</script>
+
+<svelte:window/>
+
+<section class="backdrop"><P5 {sketch} /></section>
+
+<style>
+	.backdrop {
+		grid-area: 1 / 1;
+		position: absolute;
+		left: 0; 
+		top: 0;
+		z-index: -1;
+		width: 500px;
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,41 +1,6 @@
 <script lang="ts">
 	import Nav from '../components/+nav.svelte';
-	import P5, { type Sketch } from 'p5-svelte';
-
-	const sketch: Sketch = (p5) => {
-		let y = 100;
-		let z = 100;
-		let q = 100;
-		const padding = 8;
-		// The statements in the setup() function
-		// execute once when the program begins
-		p5.setup = () => {
-			// createCanvas must be the first statement
-			p5.createCanvas(p5.windowWidth - padding, p5.windowHeight - padding);
-			p5.stroke(247, 184, 1); // Set line drawing color to white
-			p5.frameRate(160);
-		};
-		// The statements in draw() are executed until the
-		// program is stopped. Each statement is executed in
-		// sequence and after the last line is read, the first
-		// line is executed again.
-		p5.draw = () => {
-			p5.background(48, 50, 61); // Set the background to black
-
-			y = createLines(y, 10);
-			q = createLines(q, 5);
-			z = createLines(z, 7.5);
-		};
-		function createLines (linePoint: number, steps: number) {
-			linePoint -= steps
-			if (linePoint < 0) {
-				linePoint = p5.height
-			}
-			p5.line(0, linePoint, p5.width, linePoint);
-			return linePoint;
-		}
-
-	};
+	import Backdrop from '../components/+backdrop.svelte';
 </script>
 
 <svelte:window/>
@@ -43,21 +8,13 @@
 <Nav />
 
 <div>
-	<section class="layout__backdrop"><P5 {sketch} /></section>
+	<Backdrop/>
 	<main class="layout__main">
 		<slot />
 	</main>
 </div>
 
 <style>
-	.layout__backdrop {
-		grid-area: 1 / 1;
-		position: absolute;
-		left: 0; 
-		top: 0;
-		z-index: -1;
-		width: 500px;
-	}
 	.layout__main {
 		display: grid;
 		grid-template-columns: 1fr;

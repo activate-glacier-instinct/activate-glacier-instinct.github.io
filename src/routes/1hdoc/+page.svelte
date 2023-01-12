1c18dde4e1a109ec448e078e53fe1ea3c8ce0873
activate-glacier-instinct <101533082+activate-glacier-instinct@users.noreply.github.com>
2023-01-12 23:55:23 +0000

    FEAT : adds more content to the /1hdoc page
    Created a summary page for 100days-of-code challenge

--- src/routes/1hdoc/+page.svelte
+++ src/routes/1hdoc/+page.svelte
@@ -1,5 +1,22 @@
-<h1>100days-of-code Challenge</h1>
-<p>2023</p>
+<h1 class="accent">100days-of-code Challenge</h1>
+<p>Started on <time datetime="6:22 PM · Jan 7, 2023">7th January 2023</time></p>
+<p>
+	My plan for the challenge can be found <a
+		target="_blank"
+		href="https://github.com/activate-glacier-instinct/100-days-of-code/blob/master/plan.md"
+		>here (Github)</a
+	>
+</p>
+<p>
+	<a target="_blank" href="https://twitter.com/ActivateGlacier"
+		>Twitter</a
+	> is where I post my updates
+</p>
+<p>
+	All projects can be found on <a target="_blank" href="https://github.com/activate-glacier-instinct"
+		>my Github account</a
+	>
+</p>
 
 <style>
 	h1,

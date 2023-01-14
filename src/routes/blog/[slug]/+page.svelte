67faf670f3c77ff5f4d3e5d07b27a8f340f64866
activate-glacier-instinct <101533082+activate-glacier-instinct@users.noreply.github.com>
2023-01-14 20:29:11 +0000

    FEAT (blog/posts) : adds logic for generating blog pages
    Adds a bit of dummy json to signify the future pages
    
    Adds a load function that "loads" data and defined 404s if data is not found.
    
    Add component logic to fetch this data and render it as html

---
+++ src/routes/blog/[slug]/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	/** @type {import('./$types').PageData} */
+
+	import type { PageData } from './$types';
+	export let data: PageData;
+	const { post } = data;
+	
+</script>
+
+<h1 class="accent">{post && post.title}</h1>
+<div>{@html post?.body}</div>
+
+<!-- TODO : read more about this  -->
+<!-- <svelte:component this={post} /> -->
+
+<style>
+	h1,
+	h2,
+	p {
+		font-size: 2rem;
+		font-weight: 900;
+	}
+	.accent {
+		font-style: normal;
+		color: var(--theme-color-mango);
+	}
+</style>
---
+++ src/routes/blog/[slug]/+page.ts
@@ -0,0 +1,22 @@
+
+import type { PageLoad } from './$types';
+import { error } from '@sveltejs/kit';
+import blogPosts from './data.json'
+
+type BlogPost = {
+  title: string,
+  body: string
+}
+
+export const load: PageLoad = async function load({ params, url }) {
+  const { slug } = params;
+  const { pathname } = url;
+
+  const post = blogPosts.filter((post: BlogPost) => post.title === slug)[0]
+  if (!post) throw error(404, 'Not found!')
+
+  return {
+    post
+  };
+
+};

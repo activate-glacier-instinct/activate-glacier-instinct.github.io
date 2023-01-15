8b66cbfd9d05e53a82466bce2b62a289dda7305b
activate-glacier-instinct <101533082+activate-glacier-instinct@users.noreply.github.com>
2023-01-15 23:56:38 +0000

    FEAT (/blog) : add dynamic fetching from github
    Gets logs from github and renders them on the page

--- src/components/+link--blog.svelte
+++ src/components/+link--blog.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	export let title: string;
 	export const url = '/blog/' + title;
+    export let githubUrl: string
 </script>
 
-<li><a href={url}>{title}</a></li>
+<li><a href={url} data-github-url={githubUrl}>{title}</a></li>
--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -2,20 +2,30 @@
 	/** @type {import('./$types').PageData} */
 
 	import type { PageData } from './$types';
+	import type { BlogPosts } from './+page';
 	import BlogPostLink from '../../components/+link--blog.svelte';
 	export let data: PageData;
 	const { posts } = data;
+	export let loadedData: BlogPosts;
+	if (posts) {
+		loadedData = posts
+	}
 </script>
 
-<h1 class="accent">Blog</h1>
-<p class="subtitle">
-	Read more of my articles below:
-</p>
-<ul class="urlList">
-	{#each posts as post}
-		<BlogPostLink title={post.title}/>
-	{/each}
-</ul>
+{#await posts}
+	<!-- optionally show something while promise is pending -->
+{:then posts}
+	<!-- promise was fulfilled -->
+	<h1 class="accent">Blog</h1>
+	<p class="subtitle">Read more of my articles below:</p>
+	<ul class="urlList">
+		{#each loadedData as post}
+			<BlogPostLink title={post.name} githubUrl={post.html_url} />
+		{/each}
+	</ul>
+{:catch error}
+	<!-- optionally show something while promise was rejected -->
+{/await}
 
 <style>
 	h1,

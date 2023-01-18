322af2686add32d03dc7ead4ed08515b34a828dd
activate-glacier-instinct <101533082+activate-glacier-instinct@users.noreply.github.com>
2023-01-18 00:20:28 +0000

    REFACTOR: moved the components back out of the lib

--- src/lib/components/+link--blog.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	export let title: string;
-	export const url = '/blog/' + title;
-    export let githubUrl: string
-</script>
-
-<li><a href={url} data-github-url={githubUrl}>{title}</a></li>

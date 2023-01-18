a15d956f594734ede795e71191fde6a1852a94bd
activate-glacier-instinct <101533082+activate-glacier-instinct@users.noreply.github.com>
2023-01-18 00:21:13 +0000

    FIX (blog) : removed most of the fetching functionality

--- src/routes/blog/+layout.svelte
+++ src/routes/blog/+layout.svelte
@@ -1,8 +1,5 @@
 <script lang="ts">
     /** @type {import('./$types').PageData} */
-    export let data: PageData;
-    import type { PageData } from './$types';
-	const { posts } = data;
 </script>
 
 <slot />

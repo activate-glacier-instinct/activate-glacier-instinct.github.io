992f546ebeb34ae96be4762773067104e5634205
activate-glacier-instinct <101533082+activate-glacier-instinct@users.noreply.github.com>
2023-01-20 00:28:52 +0000

    FEAT (packages) : adds vite-plugin-markdown

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { plugin as markdown } from 'vite-plugin-markdown'
+import { Mode } from 'vite-plugin-markdown'
 import type { UserConfig } from 'vite';
 
+// markdown({ mode: [Mode.HTML]})
 const config: UserConfig = {
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), markdown({ mode: [Mode.HTML] })],
+	// assetsInclude: ['**/*.md'],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}

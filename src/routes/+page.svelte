fb3974281c7c53f9165cea73b75ca2f0edcd8a69
activate-glacier-instinct <101533082+activate-glacier-instinct@users.noreply.github.com>
2023-01-22 01:36:44 +0000

    REFACTOR (100daysofcode) : moved the summary page to /blog
    Previously a contained page
    
    Now nested within the /blog
    
    Updated the links to the new page on / {homepage}

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 	<h1>I'm <i class="accent">activate-glacier-instinct</i>,</h1>
 	<h2>Fullstack Web Developer learning smart contract development with Solidity</h2>
 	<p>
-		Read about my experience with <a href="/1hdoc">100days-of-code</a>
+		Read about my experience with <a href="/blog/100daysofcode/">100days-of-code</a>
 	</p>
 </section>
 

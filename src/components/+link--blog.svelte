60da1e528bf96b5c96a4829b4c908ff681ece330
activate-glacier-instinct <101533082+activate-glacier-instinct@users.noreply.github.com>
2023-01-22 02:07:20 +0000

    FIX (/blog) : fixes missing training slash from /blog/100days-of-code url
    It was causing an error that could not preload the pages

--- src/components/+link--blog.svelte
+++ src/components/+link--blog.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	export let title: string;
 	export let parsedTitle:string = title.split('.')[0];
-	export const url = '/blog/' + parsedTitle;
+	export const url = '/blog/' + parsedTitle + '/';
 </script>
 
 <li><a href={url}>{parsedTitle}</a></li>

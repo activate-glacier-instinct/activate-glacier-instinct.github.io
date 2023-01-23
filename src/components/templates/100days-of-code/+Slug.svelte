a1f9c26c1d87ded3752f9477ef0d49250ab903a3
activate-glacier-instinct <101533082+activate-glacier-instinct@users.noreply.github.com>
2023-01-23 00:20:27 +0000

    FEAT (comp/templates): adds a new 100days-of-code Slug template component
    Refactored the old /100days-of-code/[slug] page
    
    Removed all previous html and styles
    
    Moved these styles to the new comp/templates/100days-of-code/Slug comp

---
+++ src/components/templates/100days-of-code/+Slug.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	/** @type {import('./$types').PageData} */
+
+	export let html: string;
+</script>
+
+<article class="page article">{@html html}</article>
+
+<style>
+	.article {
+		font-size: 1.5rem;
+		font-weight: 700;
+		position: relative;
+		top: 0;
+		height: max-content;
+	}
+	.article :global(h2) {
+		font-size: 5rem;
+		font-style: normal;
+		color: var(--theme-color-mango);
+		margin: 2rem 0;
+	}
+	.article :global(h3) {
+		font-size: 2rem;
+		font-weight: 900;
+		font-style: italic;
+		color: var(--theme-color-mango);
+	}
+	.article :global(hr) {
+		background-color: var(--theme-color-tangerine);
+		border: none;
+		height: 4px;
+	}
+
+	.article :global(li) {
+		font-size: 1.5rem;
+		font-weight: 900;
+	}
+
+	.article :global(a) {
+		font-size: 1.5rem;
+		font-weight: 900;
+		text-decoration: none;
+		color: var(--theme-color-tangerine);
+	}
+</style>
--- src/routes/blog/100days-of-code/[slug]/+page.svelte
+++ src/routes/blog/100days-of-code/[slug]/+page.svelte
@@ -2,46 +2,9 @@
 	/** @type {import('./$types').PageData} */
 
 	import type { PageData } from './$types';
+	import SlugPageTemplete from '../../../../components/templates/100days-of-code/+Slug.svelte'
 	export let data: PageData;
 </script>
 
-<article class="page article">{@html data?.html}</article>
+<SlugPageTemplete html={data.html}/>
 
-<style>
-	.article {
-		font-size: 1.5rem;
-		font-weight: 700;
-		position: relative;
-		top: 0;
-		height: max-content;
-	}
-	.article :global(h2) {
-		font-size: 5rem;
-		font-style: normal;
-		color: var(--theme-color-mango);
-		margin: 2rem 0;
-	}
-	.article :global(h3) {
-		font-size: 2rem;
-		font-weight: 900;
-		font-style: italic;
-		color: var(--theme-color-mango);
-	}
-	.article :global(hr) {
-		background-color: var(--theme-color-tangerine);
-		border: none;
-		height: 4px;
-	}
-
-	.article :global(li) {
-		font-size: 1.5rem;
-		font-weight: 900;
-	}
-
-	.article :global(a) {
-		font-size: 1.5rem;
-		font-weight: 900;
-		text-decoration: none;
-		color: var(--theme-color-tangerine);
-	}
-</style>

5a7b9c6b20669a8169306f295488fad24076012e
activate-glacier-instinct <101533082+activate-glacier-instinct@users.noreply.github.com>
2023-01-23 00:37:57 +0000

    Deploying to gh-pages from  @ 243de56bc9c750ab4b05ec308d5a108b1a099b43 🚀

---
+++ _app/immutable/chunks/day16-34cb4931.js
@@ -0,0 +1,29 @@
+const t={},n=`<h2>Day 16/100</h2>
+<hr>
+<p>Date: 22/01/23</p>
+<hr>
+<h3>Goal:</h3>
+<p>Make a simple portfolio + blog site using SvelteKit for my anon account</p>
+<h3><strong>Today's Progress</strong>:</h3>
+<ul>
+<li>Finalised the /blog/100days-of-code/[slug] designs, aka the logs pages</li>
+<li>Learned about how to style injected html content with Svelte</li>
+<li>Investigated creating web-components with Svelte, but ended up not using it</li>
+<li>Organised components under atomic design principles</li>
+<li>Bunch of code cleanup</li>
+</ul>
+<h3><strong>Thoughts</strong>:</h3>
+<ul>
+<li>Creating native web-components with Svelte seems interesting and useful. Glad that I've given this a try.</li>
+</ul>
+<h3><strong>Next</strong>:</h3>
+<ul>
+<li>Improve the /work page with better content and examples</li>
+</ul>
+<h3><strong>Link to work:</strong></h3>
+<ul>
+<li><a href="https://github.com/activate-glacier-instinct/activate-glacier-instinct.github.io">Portfolio - Repo</a></li>
+<li><a href="https://activate-glacier-instinct.github.io/">Portfolio - Deployment</a></li>
+<li><a href="https://www.figma.com/file/EACX3PwCLrEc2q3oHRtxU4/Portfolio---Moodboard?node-id=0%3A1">Portfolio - Design Moodboard</a></li>
+</ul>
+`;export{t as attributes,n as html};

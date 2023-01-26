12bab59868bb05250ee93afa2b7abdf3b620324f
activate-glacier-instinct <101533082+activate-glacier-instinct@users.noreply.github.com>
2023-01-26 00:14:00 +0000

    FEAT (/work) : adds content and dynamic rendering from content
    Adds content for the work page
    
    Page generates elements from this data now
    
    Removed redundant code from /blog/page.ts

--- src/routes/work/+page.svelte
+++ src/routes/work/+page.svelte
@@ -1,6 +1,22 @@
+<script lang="ts">
+	/** @type {import('./$types').PageData} */
+
+	import type { PageData } from './$types';
+	export let data: PageData;
+	export const { cards } = data;
+</script>
+
 <section class="page page--center">
 	<h1 class="accent">Work Experience</h1>
-	<p>Bankless DAO: Bankless Loans</p>
-	<p>Bankless DAO: Bankless DAO Website</p>
-	<p>Bankless DAO: Smart Contract Book Club</p>
+	<ul>
+		{#each cards as card}
+			<li>
+				<article>
+					<p>{card.title}</p>
+					<p>{card.where}</p>
+					<p>{card.type}</p>
+				</article>
+			</li>
+		{/each}
+	</ul>
 </section>
--- src/routes/work/+page.ts
+++ src/routes/work/+page.ts
@@ -1,9 +1,9 @@
 import type { PageLoad } from './$types';
-import blogPages from '../../content/blog/pages.json'
+import cardsData from '../../content/work/cards.json'
 
 export const load: PageLoad = async function load() {
     return {
-        posts: blogPages
+        cards: cardsData
     }
 };
 

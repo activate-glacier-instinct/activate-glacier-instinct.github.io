b5613f31caab1ad7729985589a13eb74a0f42844
activate-glacier-instinct <101533082+activate-glacier-instinct@users.noreply.github.com>
2023-01-26 01:21:21 +0000

    Deploying to gh-pages from  @ c90b492b4496e2084adb24c45be013b9977d5df0 🚀

---
+++ _app/immutable/chunks/day19-d993e403.js
@@ -0,0 +1,29 @@
+const t={},o=`<h2>Day 19/100</h2>
+<hr>
+<p>Date: 25/01/23</p>
+<hr>
+<h3>Goal:</h3>
+<p>Make a simple portfolio + blog site using SvelteKit for my anon account</p>
+<h3><strong>Today's Progress</strong>:</h3>
+<ul>
+<li>Ended the break by finishing up the portfolio</li>
+<li>Added a card-based design to the /work page</li>
+<li>Adds a data file for the /work cards</li>
+<li>Adds component rendering from this new /work cards data json</li>
+<li>Adds components, styling, and adjusted the layout</li>
+</ul>
+<h3><strong>Thoughts</strong>:</h3>
+<ul>
+<li>That's it, tomorrow is the last day of this project as I'm moving over to smart contract work with LearnWeb3 DAO Sophomore track</li>
+</ul>
+<h3><strong>Next</strong>:</h3>
+<ul>
+<li>Finish the /work cards</li>
+</ul>
+<h3><strong>Link to work:</strong></h3>
+<ul>
+<li><a href="https://github.com/activate-glacier-instinct/activate-glacier-instinct.github.io">Portfolio - Repo</a></li>
+<li><a href="https://activate-glacier-instinct.github.io/">Portfolio - Deployment</a></li>
+<li><a href="https://www.figma.com/file/EACX3PwCLrEc2q3oHRtxU4/Portfolio---Moodboard?node-id=0%3A1">Portfolio - Design Moodboard</a></li>
+</ul>
+`;export{t as attributes,o as html};
---
+++ _app/immutable/modules/pages/blog/100days-of-code/_page.ts-4bf54d98.js
@@ -0,0 +1,1 @@
+import"../../../../chunks/preload-helper-41c905a7.js";import{l,p as o,t as p}from"../../../../chunks/_page-84970f93.js";export{l as load,o as prerender,p as trailingSlash};

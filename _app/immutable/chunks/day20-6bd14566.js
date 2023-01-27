6c5185d6fee99c05110cbfad288ee37d5a29d788
activate-glacier-instinct <101533082+activate-glacier-instinct@users.noreply.github.com>
2023-01-27 01:54:33 +0000

    Deploying to gh-pages from  @ 508126f251d3072e4ee9e4958fec0e5c213bd4c9 🚀

---
+++ _app/immutable/chunks/day20-6bd14566.js
@@ -0,0 +1,27 @@
+const t={},o=`<h2>Day 20/100</h2>
+<hr>
+<p>Date: 26/01/23</p>
+<hr>
+<h3>Goal:</h3>
+<p>Make a simple portfolio + blog site using SvelteKit for my anon account</p>
+<h3><strong>Today's Progress</strong>:</h3>
+<ul>
+<li>Slow day today, short day too</li>
+<li>Researched 3D tilt effects for the cards</li>
+<li>Implemented two approaches, but need to push further to resolve Typescript of Svelte specific bugs</li>
+</ul>
+<h3><strong>Thoughts</strong>:</h3>
+<ul>
+<li>Decided to cut this short after a lack of productivity was noticed. Sometimes when you're too tired to solve a problem, you should probably just go to bed.</li>
+</ul>
+<h3><strong>Next</strong>:</h3>
+<ul>
+<li>Finish the /work cards</li>
+</ul>
+<h3><strong>Link to work:</strong></h3>
+<ul>
+<li><a href="https://github.com/activate-glacier-instinct/activate-glacier-instinct.github.io">Portfolio - Repo</a></li>
+<li><a href="https://activate-glacier-instinct.github.io/">Portfolio - Deployment</a></li>
+<li><a href="https://www.figma.com/file/EACX3PwCLrEc2q3oHRtxU4/Portfolio---Moodboard?node-id=0%3A1">Portfolio - Design Moodboard</a></li>
+</ul>
+`;export{t as attributes,o as html};
--- _app/immutable/modules/pages/blog/100days-of-code/_slug_/_page.ts-22abb581.js
+++
@@ -1,1 +0,0 @@
-import"../../../../../chunks/preload-helper-41c905a7.js";import{l,p as o,t as p}from"../../../../../chunks/_page-ad08f0a4.js";export{l as load,o as prerender,p as trailingSlash};

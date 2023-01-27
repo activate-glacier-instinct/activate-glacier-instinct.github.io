ed7dd9d35626d37199bfb29c46c63fb2cc741ff8
activate-glacier-instinct <101533082+activate-glacier-instinct@users.noreply.github.com>
2023-01-27 01:46:06 +0000

    WIP (card 3d tilt effect) : trying a few different things here
    No real results

--- src/components/molecules/Card/+Card.svelte
+++ src/components/molecules/Card/+Card.svelte
@@ -1,51 +1,55 @@
 <script lang="ts">
-    type Card = {
-        title: string;
-        where: string;
-        type: string;
-        cardSize: string;
-    }
+	import VanillaTilt from 'vanilla-tilt';
+	type Card = {
+		title: string;
+		where: string;
+		type: string;
+		cardSize: string;
+	};
 	export let cardData: Card;
+	// VanillaTilt.init(document.querySelectorAll('.card'), {
+	// 	glare: true
+	// });
 </script>
 
-<li class="card {cardData.cardSize}">
-    <article class="card-container">
-        <p>{cardData.title}</p>
-        <p>{cardData.where}</p>
-        <p>{cardData.type}</p>
-    </article>
+<li class="card {cardData.cardSize}" data-tilt>
+	<article class="card-container">
+		<p>{cardData.title}</p>
+		<p>{cardData.where}</p>
+		<p>{cardData.type}</p>
+	</article>
 </li>
 
 <style>
-    .card {
-        height: 100%;
-        background-color: #1f1f1f;
-        color: var(--theme-color-purple-light);
-        min-width: 0;
-    }
-    .card.hero {
-        grid-column: span 2;
-        grid-row: span 2;
-    }
-    .card.regular {
-        grid-column: span 1;
-        grid-row: span 2;
-    }
-    .card.small {
-        grid-column: span 1;
-        grid-row: span 1;
-    }
-    .card.full {
-        grid-column: span 3;
-        grid-row: span 2;
-    }
-    .card-container {
-        display: flex;
-        flex-direction: column;
-        height: fit-content;
-        margin: 1rem
-    }
-    .card-container > p{
-        margin: 0.5rem 0rem
-    }
+	.card {
+		height: 100%;
+		background-color: #1f1f1f;
+		color: var(--theme-color-purple-light);
+		min-width: 0;
+	}
+	.card.hero {
+		grid-column: span 2;
+		grid-row: span 2;
+	}
+	.card.regular {
+		grid-column: span 1;
+		grid-row: span 2;
+	}
+	.card.small {
+		grid-column: span 1;
+		grid-row: span 1;
+	}
+	.card.full {
+		grid-column: span 3;
+		grid-row: span 2;
+	}
+	.card-container {
+		display: flex;
+		flex-direction: column;
+		height: fit-content;
+		margin: 1rem;
+	}
+	.card-container > p {
+		margin: 0.5rem 0rem;
+	}
 </style>
--- src/routes/work/+page.svelte
+++ src/routes/work/+page.svelte
@@ -2,18 +2,27 @@
 	/** @type {import('./$types').PageData} */
 
 	import type { PageData } from './$types';
+	import VanillaTilt from 'vanilla-tilt';
+	import { onMount } from 'svelte';
 	export let data: PageData;
-
+	export let rootList;
+	import Tilt from "preact-tilt";
 	import Card from '../../components/molecules/Card/+Card.svelte';
 	export const { cards } = data;
-	
+
+	onMount(() => {
+		let cards: Array<HTMLElement> = document.querySelectorAll('.card') as Array<HTMLElement>
+		VanillaTilt.init(cards, {
+			glare: true
+		});
+	});
 </script>
 
-<section class="page screen">
+<section class="page screen" bind:this={rootList}>
 	<h1 class="accent">Work Experience</h1>
 	<ul class="card-list">
 		{#each cards as card}
-			<Card cardData={card}/>
+			<Card cardData={card} />
 		{/each}
 	</ul>
 </section>

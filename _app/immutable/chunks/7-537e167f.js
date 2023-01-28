62224fe0dcecf948ce1835cf0dad319d7f8982ad
activate-glacier-instinct <101533082+activate-glacier-instinct@users.noreply.github.com>
2023-01-28 01:13:53 +0000

    Deploying to gh-pages from  @ 08704813d4e87db02b3bcf5281fc82727eab20ae 🚀

---
+++ _app/immutable/chunks/7-537e167f.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_page-27871e2e.js";import{default as t}from"../components/pages/work/_page.svelte-f4d59eea.js";export{t as component,r as universal};
---
+++ _app/immutable/chunks/day21-b662abac.js
@@ -0,0 +1,26 @@
+const e={},t=`<h2>Day 21/100</h2>
+<hr>
+<p>Date: 27/01/23</p>
+<hr>
+<h3>Goal:</h3>
+<p>Make a simple portfolio + blog site using SvelteKit for my anon account</p>
+<h3><strong>Today's Progress</strong>:</h3>
+<ul>
+<li>Implemented the 3D card tilt effect</li>
+<li>Added an Ethereum gradient shine inspired by the open design docs for the Ethereum website</li>
+<li>Added smooth animations</li>
+<li>Removed cruft</li>
+</ul>
+<h3><strong>Thoughts</strong>:</h3>
+<ul>
+<li>The Ethereum website abides by open-design principles and I used their Figma to define an Ethereum gradient https://www.figma.com/file/NrNxGjBL0Yl1PrNrOT8G2B/ethereum.org-Design-System?node-id=71%3A517&amp;t=9eyemU9f7sZsu5Ki-0</li>
+<li>There's more to do on the cards: maybe add links to the work, then the work should each have a page</li>
+<li>However I am going to move on to the next project from tomorrow. Happy with the portfolio as it is for a few months.</li>
+</ul>
+<h3><strong>Link to work:</strong></h3>
+<ul>
+<li><a href="https://github.com/activate-glacier-instinct/activate-glacier-instinct.github.io">Portfolio - Repo</a></li>
+<li><a href="https://activate-glacier-instinct.github.io/">Portfolio - Deployment</a></li>
+<li><a href="https://www.figma.com/file/EACX3PwCLrEc2q3oHRtxU4/Portfolio---Moodboard?node-id=0%3A1">Portfolio - Design Moodboard</a></li>
+</ul>
+`;export{e as attributes,t as html};

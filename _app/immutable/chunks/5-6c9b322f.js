321c41af3fe9c9a89f1dcdb2ef9464a6cb687a08
activate-glacier-instinct <101533082+activate-glacier-instinct@users.noreply.github.com>
2023-01-31 23:04:37 +0000

    Deploying to gh-pages from  @ f6d37e9df3799342fdc098e391f0e2c000bc68dc 🚀

---
+++ _app/immutable/chunks/5-6c9b322f.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_page-9253f318.js";import{default as t}from"../components/pages/blog/100days-of-code/_page.svelte-523139c9.js";export{t as component,r as universal};
---
+++ _app/immutable/chunks/day25-5a33469d.js
@@ -0,0 +1,35 @@
+const t={},o=`<h2>Day 25/100</h2>
+<hr>
+<p>Date: 31/01/23</p>
+<hr>
+<h3>Goal:</h3>
+<p>Build a full whitelist dApp in Foundry - LW3 Sophomore track</p>
+<h3><strong>Today's Progress</strong>:</h3>
+<ul>
+<li>Foundry/forge: adds coverage report (100%)</li>
+<li>Foundry/anvil--local: started a local network</li>
+<li>Foundry/anvil--local: deployed the contract locally</li>
+<li>Foundry/cast--local: tested the deployed contract using cast</li>
+<li>Foundry/anvil--local: adds docs for this here - <a href="https://github.com/activate-glacier-instinct/foundry-whitelist-dapp--lw3#local-deploy--testing-anvil">Local Deploy / Testing (Anvil)</a></li>
+<li>Foundry/anvil--goerli: configured anvil to deploy to goerli</li>
+<li>Foundry/anvil--goerli: adds docs for this here - <a href="https://github.com/activate-glacier-instinct/foundry-whitelist-dapp--lw3#deploying---goerli--alchemy-rpc">Deploying - Goerli / Alchemy RPC</a></li>
+<li>Foundry/anvil--goerli: deployed the contract to Goerli</li>
+<li>Foundry/cast--goerli: tested the deployed contract using cast</li>
+<li>LearnWeb3: Completed Sophomore / Lesson 9 - verified the Goerli contract</li>
+</ul>
+<h3><strong>Thoughts</strong>:</h3>
+<ul>
+<li>Explore Foundry wallet production setups - <a href="https://book.getfoundry.sh/reference/forge/forge-script.html#wallet-options---raw">foundry book</a></li>
+<li>Useful Foundry resources from Nader Dabit - <a href="https://github.com/dabit3/foundry-workshop">Foundry Workshop</a> and <a href="https://github.com/dabit3/foundry-cheatsheet">Foundry Cheatsheet</a></li>
+<li>Not sure if I still want to do the dApp using NextJS or using SvelteKit. Don't think I've seen any SvelteKit dApps yet soooo.....</li>
+</ul>
+<h3><strong>Next up</strong>:</h3>
+<ul>
+<li>Frontend for the dApp</li>
+</ul>
+<h3><strong>Link to work:</strong></h3>
+<ul>
+<li><a href="https://github.com/activate-glacier-instinct/foundry-whitelist-dapp--lw3">Whitelist dApp (contract) - Project Repo</a></li>
+<li><a href="https://activate-glacier-instinct.github.io/">My Portfolio</a></li>
+</ul>
+`;export{t as attributes,o as html};

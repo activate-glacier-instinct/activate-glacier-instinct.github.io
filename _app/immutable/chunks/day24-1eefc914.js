3bfd0907a0452521ebd766731fa0d5e66358b258
activate-glacier-instinct <101533082+activate-glacier-instinct@users.noreply.github.com>
2023-01-31 01:22:03 +0000

    Deploying to gh-pages from  @ be83a1b9dc45de4c462ee372508ed7881f903413 🚀

---
+++ _app/immutable/chunks/day24-1eefc914.js
@@ -0,0 +1,27 @@
+const t={},o=`<h2>Day 24/100</h2>
+<hr>
+<p>Date: 30/01/23</p>
+<hr>
+<h3>Goal:</h3>
+<p>Build a full whitelist dApp in Foundry - LW3 Sophomore track</p>
+<h3><strong>Today's Progress</strong>:</h3>
+<ul>
+<li>Foundry: adds Whitelist contract</li>
+<li>Foundry: learned how to write tests, how to test reverts, how to mock addresses, how to assert</li>
+<li>Foundry: adds tests for Whitelist contract methods and properties.</li>
+</ul>
+<h3><strong>Thoughts</strong>:</h3>
+<ul>
+<li>Should attempt to make an MR on Foundry on the issue I raised here https://github.com/foundry-rs/foundry/issues/4215. A contributor recommended it as a good issues, and offered some pointers.</li>
+<li>Surprisingly straightforward to get up to speed with Foundry using the Foundry book https://book.getfoundry.sh/forge/writing-tests</li>
+</ul>
+<h3><strong>Next up</strong>:</h3>
+<ul>
+<li>Deployment of the contract</li>
+</ul>
+<h3><strong>Link to work:</strong></h3>
+<ul>
+<li><a href="https://github.com/activate-glacier-instinct/foundry-whitelist-dapp--lw3">Project Repo</a></li>
+<li><a href="https://activate-glacier-instinct.github.io/">My Portfolio</a></li>
+</ul>
+`;export{t as attributes,o as html};

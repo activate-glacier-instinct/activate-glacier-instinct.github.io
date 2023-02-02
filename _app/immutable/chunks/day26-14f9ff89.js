d12e0ac906c8d34c5f9016e66039eb8d7005114a
activate-glacier-instinct <101533082+activate-glacier-instinct@users.noreply.github.com>
2023-02-02 00:30:41 +0000

    Deploying to gh-pages from  @ b83db9eeb3a6854946337f5468c12f5c742ef6fc 🚀

---
+++ _app/immutable/chunks/day26-14f9ff89.js
@@ -0,0 +1,26 @@
+const t={},e=`<h2>Day 26/100</h2>
+<hr>
+<p>Date: 01/02/23</p>
+<hr>
+<h3>Goal:</h3>
+<p>Build a full whitelist dApp in SvelteKit- LW3 Sophomore track</p>
+<h3><strong>Today's Progress</strong>:</h3>
+<ul>
+<li>SvelteKit: setup the scaffold, after getting more votes for SvelteKit over NextJS</li>
+<li>Created the repo</li>
+<li>Adds the lesson design</li>
+<li>Started looking into the integration with web3</li>
+</ul>
+<h3><strong>Thoughts</strong>:</h3>
+<ul>
+<li>Leaning more towards a plain JS implementation within Svelte vs a more Svelte native option</li>
+<li>Nice example of web3 svelte dApp <a href="https://github.com/clbrge/svelte-web3/blob/master/examples/svelte-app-template-web3/src/App.svelte">here</a> as part of <a href="https://www.npmjs.com/package/svelte-web3?activeTab=readme">svelte-web3</a></li>
+<li><a href="https://docs.walletconnect.com/2.0/web3modal/html-js/installation">WalletConnect docs</a> also look good</li>
+</ul>
+<h3><strong>Link to work:</strong></h3>
+<ul>
+<li><a href="https://github.com/activate-glacier-instinct/whitelist-dapp-svelte--lw3">Whitelist (dApp) - Project Repo</a></li>
+<li><a href="https://github.com/activate-glacier-instinct/whitelist-contract-foundry--lw3">Whitelist (contract) - Project Repo</a></li>
+<li><a href="https://activate-glacier-instinct.github.io/">My Portfolio</a></li>
+</ul>
+`;export{t as attributes,e as html};
---
+++ _app/immutable/modules/pages/blog/100days-of-code/_page.ts-1b8f353f.js
@@ -0,0 +1,1 @@
+import"../../../../chunks/preload-helper-41c905a7.js";import{l,p as o,t as p}from"../../../../chunks/_page-f67d7d99.js";export{l as load,o as prerender,p as trailingSlash};

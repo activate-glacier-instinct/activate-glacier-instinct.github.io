37da5a1fd6888bee4494eda26d8f9c4ca4bb23c7
activate-glacier-instinct <101533082+activate-glacier-instinct@users.noreply.github.com>
2023-02-03 01:25:59 +0000

    Deploying to gh-pages from  @ 8b2a3f709502a75a189d7cda9522f1fd37e56bbf 🚀

--- _app/immutable/chunks/6-acf70fea.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./_page-77c6c4d4.js";import{default as t}from"../components/pages/blog/100days-of-code/_slug_/_page.svelte-f7265731.js";export{t as component,r as universal};
---
+++ _app/immutable/chunks/day27-170311b5.js
@@ -0,0 +1,27 @@
+const e={},t=`<h2>Day 27/100</h2>
+<hr>
+<p>Date: 02/02/23</p>
+<hr>
+<h3>Goal:</h3>
+<p>Build a full whitelist dApp in SvelteKit- LW3 Sophomore track</p>
+<h3><strong>Today's Progress</strong>:</h3>
+<ul>
+<li>Implemented the NextJS example in Svelte</li>
+<li>Learned more aboute WalletConnect/web3modal</li>
+<li>Svelte: Learned more about stores, contexts, conditional rendering, lifecycle methods</li>
+</ul>
+<h3><strong>Thoughts</strong>:</h3>
+<ul>
+<li>Spent too long dissecting how the web3Modal works using <a href="https://codesandbox.io/s/j43b10?file=/src/App.js">this implementation</a>, referenced in the <a href="https://github.com/WalletConnect/web3modal">WalletConnect/web3modal repo</a></li>
+<li>Spent time looking at the Svelte native implementation svelte-web3 - <a href="https://github.com/clbrge/svelte-web3">repo</a></li>
+<li>Nearly went with this <a href="https://github.com/clbrge/svelte-web3/tree/master/examples/sveltekit-app-template-web3">svelte-web3 example</a></li>
+<li>Then I just realised that I'm complicating things, so I came back to the given NextJS example and just tweaked to work with the Svelte api</li>
+<li>Used this repo to find <code>React to Svelte</code> hook / lifecycle mappings - <a href="https://github.com/joshnuss/react-hooks-in-svelte">react-hooks-in-svelte</a></li>
+</ul>
+<h3><strong>Link to work:</strong></h3>
+<ul>
+<li><a href="https://github.com/activate-glacier-instinct/whitelist-dapp-svelte--lw3">Whitelist (dApp) - Project Repo</a></li>
+<li><a href="https://github.com/activate-glacier-instinct/whitelist-contract-foundry--lw3">Whitelist (contract) - Project Repo</a></li>
+<li><a href="https://activate-glacier-instinct.github.io/">My Portfolio</a></li>
+</ul>
+`;export{e as attributes,t as html};

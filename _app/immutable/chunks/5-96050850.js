c7558bde2389da13808acd1d9c5e2df9aecabe36
activate-glacier-instinct <101533082+activate-glacier-instinct@users.noreply.github.com>
2023-02-11 00:22:18 +0000

    Deploying to gh-pages from  @ f8fd0241f20bb78ba05c8a83f29bc24246108d0e 🚀

--- _app/immutable/chunks/5-96050850.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./_page-faa78b02.js";import{default as t}from"../components/pages/blog/100days-of-code/_page.svelte-523139c9.js";export{t as component,r as universal};
---
+++ _app/immutable/chunks/day30-e30f3ca9.js
@@ -0,0 +1,25 @@
+const t={},n=`<h2>Day 30/100</h2>
+<hr>
+<p>Date: 10/02/23</p>
+<hr>
+<h3>Goal:</h3>
+<p>Build a full NFT collection smart contract in Foundry - LW3 Sophomore track</p>
+<h3><strong>Today's Progress</strong>:</h3>
+<ul>
+<li>Created the NFT Collection scaffolding and repo</li>
+<li>Installed Openzeppelin dependencies</li>
+<li>Added the contracts</li>
+<li>Went through the <a href="https://www.youtube.com/watch?v=fNMfMxGxeag">Intro to Foundry - video</a> from Patrick Collins to get a wider understanding of Foundry</li>
+</ul>
+<h3><strong>Thoughts</strong>:</h3>
+<ul>
+<li>Had some issues with the remappings. It was quite annoying since installing dependencies isn't really a niche behaviour.</li>
+<li>Eventually fixed after reading the <a href="https://book.getfoundry.sh/tutorials/solmate-nft">Foundry Book</a></li>
+<li>Wondering how I'm going to achieve the IWhitelist pattern here, as in how will I be sending the Whitelist address to the <code>IWhitelist</code> interface within Foundry? Maybe using bash scripting...</li>
+</ul>
+<h3><strong>Link to work:</strong></h3>
+<ul>
+<li><a href="https://github.com/activate-glacier-instinct/nft-collection-contract-foundry--lw3">NFT Collection (contract) - Project Repo</a></li>
+<li><a href="https://activate-glacier-instinct.github.io/">My Portfolio</a></li>
+</ul>
+`;export{t as attributes,n as html};

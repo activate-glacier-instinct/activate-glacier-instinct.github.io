ec43f42f4d995ad44a3bd9bef8ae0e5fc8a7f8c3
activate-glacier-instinct <101533082+activate-glacier-instinct@users.noreply.github.com>
2023-02-12 02:21:13 +0000

    Deploying to gh-pages from  @ 09e9e74416c22b29522ca5bdcf87cf8d07a12f88 🚀

---
+++ _app/immutable/chunks/day31-6d9c7ddd.js
@@ -0,0 +1,25 @@
+const t={},n=`<h2>Day 31/100</h2>
+<hr>
+<p>Date: 11/02/23</p>
+<hr>
+<h3>Goal:</h3>
+<p>Build a full NFT collection smart contract in Foundry - LW3 Sophomore track</p>
+<h3><strong>Today's Progress</strong>:</h3>
+<ul>
+<li>Foundry: learned about cheatcodes and how to integrate bash scripts into a project</li>
+<li>Contract: started researching the contract tests</li>
+<li>Contract: looking into interfaces and using Interfaces within Foundry tests</li>
+<li>Contract: also looking into <code>string -&gt; address</code> mapping</li>
+<li>Contract: also looking into ERC721 testing and ERC20 mocking</li>
+</ul>
+<h3><strong>Thoughts</strong>:</h3>
+<ul>
+<li>Lots of learning, but no coding today!</li>
+<li>Starting to think that I'm going to alter the original <code>CryptoDevs.sol</code> a significant amount, starting with the contractor. The fact that we're passing an address to the constructor is causing problems.</li>
+</ul>
+<h3><strong>Link to work:</strong></h3>
+<ul>
+<li><a href="https://github.com/activate-glacier-instinct/nft-collection-contract-foundry--lw3">NFT Collection (contract) - Project Repo</a></li>
+<li><a href="https://activate-glacier-instinct.github.io/">My Portfolio</a></li>
+</ul>
+`;export{t as attributes,n as html};

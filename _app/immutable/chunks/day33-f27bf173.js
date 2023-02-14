cf71873997568ed1c9acbb4685963ec397e7d7fb
activate-glacier-instinct <101533082+activate-glacier-instinct@users.noreply.github.com>
2023-02-14 04:14:33 +0000

    Deploying to gh-pages from  @ c8c33531cc20cc7278f8a3dc7d4e3eae7a3ac0fd 🚀

---
+++ _app/immutable/chunks/day33-f27bf173.js
@@ -0,0 +1,19 @@
+const n={},t=`<h2>Day 33/100</h2>
+<hr>
+<p>Date: 13/02/23</p>
+<hr>
+<h3>Goal:</h3>
+<p>Build a full NFT collection smart contract in Foundry - LW3 Sophomore track</p>
+<h3><strong>Today's Progress</strong>:</h3>
+<ul>
+<li>Foundry: added a Makefile for basic scripting needs</li>
+<li>Adds basic readme following <code>standard-readme</code></li>
+<li>Foundry: read up on mocks and interface contract testing</li>
+<li>Foundry: added more tests, coverage at 31%</li>
+</ul>
+<h3><strong>Link to work:</strong></h3>
+<ul>
+<li><a href="https://github.com/activate-glacier-instinct/nft-collection-contract-foundry--lw3">NFT Collection (contract) - Project Repo</a></li>
+<li><a href="https://activate-glacier-instinct.github.io/">My Portfolio</a></li>
+</ul>
+`;export{n as attributes,t as html};
--- _app/immutable/modules/pages/blog/100days-of-code/_page.ts-971a2684.js
+++
@@ -1,1 +0,0 @@
-import"../../../../chunks/preload-helper-41c905a7.js";import{l,p as o,t as p}from"../../../../chunks/_page-aaf5e8cc.js";export{l as load,o as prerender,p as trailingSlash};

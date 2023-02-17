0df64cbadaa52d63538db8f3284a73a5c2d45707
activate-glacier-instinct <101533082+activate-glacier-instinct@users.noreply.github.com>
2023-02-17 02:38:34 +0000

    Deploying to gh-pages from  @ d454c570e4b1c79b5faa4d5dea907b66c3cf62b6 🚀

--- _app/immutable/chunks/5-ad273c25.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./_page-4cc8c73d.js";import{default as t}from"../components/pages/blog/100days-of-code/_page.svelte-523139c9.js";export{t as component,r as universal};
---
+++ _app/immutable/chunks/day34-806859f3.js
@@ -0,0 +1,20 @@
+const t={},n=`<h2>Day 34/100</h2>
+<hr>
+<p>Date: 16/02/23</p>
+<hr>
+<h3>Goal:</h3>
+<p>Build a full NFT collection smart contract in Foundry - LW3 Sophomore track</p>
+<h3><strong>Today's Progress</strong>:</h3>
+<ul>
+<li>Foundry: hit a roadblock with testing the mints</li>
+<li>Foundry: removed the roadblock via mocks of the Whitelist</li>
+<li>Foundry: figured out how to instantiate a contact, get its address, then pass that to another contract which uses said address for an interface, all within Foundry test suite.</li>
+<li>Test Coverage at 36%</li>
+<li>Watched: <a href="https://youtu.be/uWR78WyPSl4">Austin Griffith - shill session with 0xTranqui</a>; good video explaining a number of Foundry features and Solidity patterns</li>
+</ul>
+<h3><strong>Link to work:</strong></h3>
+<ul>
+<li><a href="https://github.com/activate-glacier-instinct/nft-collection-contract-foundry--lw3">NFT Collection (contract) - Project Repo</a></li>
+<li><a href="https://activate-glacier-instinct.github.io/">My Portfolio</a></li>
+</ul>
+`;export{t as attributes,n as html};

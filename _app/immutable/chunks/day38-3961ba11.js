37a5cb2f9ae01a594073f805cfa320ad9406737a
activate-glacier-instinct <101533082+activate-glacier-instinct@users.noreply.github.com>
2023-02-23 01:14:58 +0000

    Deploying to gh-pages from  @ bb88a8da2e746675540e39c9554a73922e9f5b44 🚀

---
+++ _app/immutable/chunks/day38-3961ba11.js
@@ -0,0 +1,27 @@
+const t={},n=`<h2>Day 37/100</h2>
+<hr>
+<p>Date: 22/02/23</p>
+<hr>
+<h3>Goal:</h3>
+<p>Build a full NFT collection smart contract in Foundry - LW3 Sophomore track</p>
+<h3><strong>Today's Progress</strong>:</h3>
+<p>dApp</p>
+<ul>
+<li>Create the dApp using NextJS</li>
+<li>Deployed to Vercel</li>
+<li>Adds api path for tokenUri and redeploys</li>
+<li>Attempted to redeploy contract with new tokenUriBase</li>
+</ul>
+<h3><strong>Next</strong>:</h3>
+<ul>
+<li>BUG: resolve <code>-32000</code> error from deploying an updated contract</li>
+</ul>
+<h3><strong>Link to work:</strong></h3>
+<ul>
+<li><a href="https://nft-collection-dapp-nextjs-lw3.vercel.app/">NFT Collection (dapp) - Deployed</a></li>
+<li><a href="https://github.com/activate-glacier-instinct/nft-collection-dapp-nextjs--lw3">NFT Collection (dapp) - Repo</a></li>
+<li><a href="https://goerli.etherscan.io/address/0xa3289287be362448fc4f41384cd94bdfc4beae4e#code">NFT Collection (contract) - Goerli contract</a></li>
+<li><a href="https://github.com/activate-glacier-instinct/nft-collection-contract-foundry--lw3">NFT Collection (contract) - Project Repo</a></li>
+<li><a href="https://activate-glacier-instinct.github.io/">My Portfolio</a></li>
+</ul>
+`;export{t as attributes,n as html};
---
+++ _app/immutable/modules/pages/blog/100days-of-code/_page.ts-72e0e08b.js
@@ -0,0 +1,1 @@
+import"../../../../chunks/preload-helper-41c905a7.js";import{l,p as o,t as p}from"../../../../chunks/_page-b8b197e4.js";export{l as load,o as prerender,p as trailingSlash};

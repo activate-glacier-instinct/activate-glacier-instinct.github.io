abb44822f72e67713e5a19dc965938f60dc254ef
activate-glacier-instinct <101533082+activate-glacier-instinct@users.noreply.github.com>
2023-02-26 22:56:51 +0000

    Deploying to gh-pages from  @ cc89f878fc12f9149655f6980bf4791fddbbb3b3 🚀

---
+++ _app/immutable/chunks/day39-e7c366d4.js
@@ -0,0 +1,31 @@
+const t={},e=`<h2>Day 39/100</h2>
+<hr>
+<p>Date: 26/02/23</p>
+<hr>
+<h3>Goal:</h3>
+<p>Build a full NFT collection smart contract in Foundry - LW3 Sophomore track</p>
+<h3><strong>Today's Progress</strong>:</h3>
+<ul>
+<li>Debugged and resolved <code>-32000</code> error from redeploying</li>
+<li>Redeployed the contract</li>
+<li>Reployed the dApp</li>
+<li>Minted the NFT and passed the lesson on LW3</li>
+</ul>
+<h2><strong>Thoughts</strong>:</h2>
+<ul>
+<li>Surprised at just how early I am vs the roadmap, so I'm going to finish the FlipsideCrypto dashboard for the next few days</li>
+<li>Admin: made <code>plan.md</code> be the current tracker of the challenge, whilst <code>roadmap.md</code> is the future looking document.</li>
+</ul>
+<h3><strong>Next</strong>:</h3>
+<ul>
+<li>Continue the <code>flipside.crypto</code> dashboard build</li>
+</ul>
+<h3><strong>Link to work:</strong></h3>
+<ul>
+<li><a href="https://nft-collection-dapp-nextjs-lw3.vercel.app/">NFT Collection (dapp) - Deployed</a></li>
+<li><a href="https://github.com/activate-glacier-instinct/nft-collection-dapp-nextjs--lw3">NFT Collection (dapp) - Repo</a></li>
+<li><a href="https://goerli.etherscan.io/address/0xa3289287be362448fc4f41384cd94bdfc4beae4e#code">NFT Collection (contract) - Goerli contract</a></li>
+<li><a href="https://github.com/activate-glacier-instinct/nft-collection-contract-foundry--lw3">NFT Collection (contract) - Project Repo</a></li>
+<li><a href="https://activate-glacier-instinct.github.io/">My Portfolio</a></li>
+</ul>
+`;export{t as attributes,e as html};
